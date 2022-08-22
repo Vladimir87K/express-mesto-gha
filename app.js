8f71e9f2a10c944c93c16159fa27c1c806f88b95
Владимир Куклин <privet1vovik@gmail.com>
2022-08-22 07:26:17 +0300

    облако

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   .catch((e) => console.log(e));
 
 app.use((req, res, next) => {
-  console.log(`${req.method}: ${req.path} ${JSON.stringify(req.body)}`);
+  console.log(`${req.method}: ${req.path} ${JSON.stringify(req.body)}!`);
   next();
 });
 
