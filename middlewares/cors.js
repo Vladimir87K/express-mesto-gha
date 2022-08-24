f1e75bf9711f437139397dddf6f33d463d9d38d4
Владимир Куклин <privet1vovik@gmail.com>
2022-08-24 12:11:19 +0300

    деплой

---
+++ middlewares/cors.js
@@ -0,0 +1,23 @@
+// Массив доменов, с которых разрешены кросс-доменные запросы
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'localhost:3000',
+];
+
+const cors = (req, res, next) => {
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  // Если это предварительный запрос, добавляем нужные заголовки
+  if (method === 'OPTIONS') {
+  // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  // eslint-disable-next-line max-len
+  } else if (allowedCors.includes(origin)) { // проверяем, что источник запроса есть среди разрешённых
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  next();
+};
+
+module.exports = cors;
